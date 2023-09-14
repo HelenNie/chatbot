1f830fc6a7b6b4c7b0b400ace67bfa5adadf5232
Helen <helen.nie94@gmail.com>
2023-09-14 15:00:40 -0400

    fixing openAI retry issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-openai_api_key = st.sidebar.text_input('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')
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
